e137274921ee71a97306ce0263d84b9200d34301
Wasif Hossain <wasif.hx@gmail.com>
2024-10-25 17:14:50 +0600

    Complete

--- supershop.cpp
+++ supershop.cpp
@@ -2,7 +2,9 @@
 // Project  : Super Shop Billing System
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <windows.h>
+#include <string>
 using namespace std;
 
 class Bill
@@ -58,10 +60,10 @@ void additem(Bill obj)
         cout << "\t1. Add Item" << endl;
         cout << "\t2. Close" << endl;
         cout << "\tEnter Choice : ";
-        int val;
-        cin >> val;
+        int choice;
+        cin >> choice;
 
-        if (val == 1)
+        if (choice == 1)
         {
             system("cls");
             string item;
@@ -93,17 +95,79 @@ void additem(Bill obj)
                 my_file.close();
             }
         }
-        else
+        else if (choice == 2)
         {
             system("cls");
-            cout << "\t--- Back to Main Menu" << endl;
-            Sleep(2000);
+            cout << "\t--- Back to Main Menu !!" << endl;
+            Sleep(1000);
 
             close = true;
+        }
+        else
+        {
+            cout << "Invalid Entry" << endl;
         }
     }
 }
 
+void printbill()
+{
+    bool close = false;
+    while (!close)
+    {
+        int choice;
+        cout << "\t1. Total Bill" << endl;
+        cout << "\t2. Back to Menu" << endl;
+        cout << "\tEnter Choice : ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            int total = 0;
+
+            system("cls");
+
+            ifstream in("totalbill.txt");
+            if (!in)
+            {
+                cout << "Please buy something before\nchecking your Bill !!\n\n";
+            }
+            else
+            {
+            string line;
+
+            while (getline(in, line))
+            {
+                cout << line << endl;
+                stringstream ss;
+                ss << line;
+                string itemName;
+                int itemRate, itemQuant;
+                char delimiter;
+                if (ss >> itemName >> delimiter >> itemRate >> delimiter >> itemQuant)
+                {
+                    total += (itemRate * itemQuant);
+                }
+            }
+            cout << "\t--------------------------" << endl;
+            cout << "\tTotal Bill = " << total << " BDT\n\n"
+                 << endl;
+            in.close();
+            }
+        }
+        else if (choice == 2)
+        {
+            close = true;
+            system("cls");
+            cout << "--- Back to Main Menu !!" << endl;
+            Sleep(2000);
+        }
+        else
+        {
+            cout << "Invalid Entry";
+        }
+    }
+}
 // <<<---------  main function   --------->>>
 int main()
 {
@@ -122,27 +186,38 @@ int main()
         cout << "\t2. Print Total Bill" << endl;
         cout << "\t3. Exit" << endl
              << endl;
+        cout << "  *************************\n\n";
         cout << "\tEnter Choice : ";
 
-        int val;
-        cin >> val;
 
-        if (val == 1)
+        int choice;
+        cin >> choice;
+
+        if (choice == 1)
         {
             system("cls");
             additem(b1);
             Sleep(2000);
         }
-
-        if (val == 3)
+        else if (choice == 2)
+        {
+            system("cls");
+            printbill();
+        }
+        else if (choice == 3)
         {
             system("cls");
             cout << "--- Thank You Sir / Maam" << endl;
-            cout << "--- Hope to See you again";
-            Sleep(2000);
+            cout << "--- Assalamualaikum\n\n";
+            Sleep(1000);
             exit = true;
         }
+        else
+        {
+            cout << "Invalid Input - Try Again";
+        }
     }
+    remove("totalbill.txt");
 
     return 0;
 }
